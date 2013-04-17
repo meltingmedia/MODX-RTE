58da99f546610c2d0e9a7def656c2bdad08f0494
Romain <romain@melting-media.com>
2013-04-17 10:38:59 +0200

    once more

---
+++ src/rte/tinymce.php
@@ -0,0 +1,17 @@
+<?php namespace meltingmedia\rte\rte;
+
+class TinyMCE
+{
+    /** @var \modX */
+    public $modx;
+    /** @var \meltingmedia\rte */
+    public $rte;
+
+    public function __construct(\meltingmedia\rte $rte)
+    {
+        $this->rte =& $rte;
+        $this->modx =& $this->rte->modx;
+
+        $this->modx->log(\modX::LOG_LEVEL_INFO, 'in tiny loader');
+    }
+}
